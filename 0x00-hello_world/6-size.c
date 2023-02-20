20e8d9726ebfe6aa189028ee8b44876fea0e4420
Marvel_ALX <marvelozuru087@gmail.com>
2023-02-20 01:26:52 -0800

    Write the 6-size.c program

---
+++ 0x00-hello_world/6-size.c
@@ -0,0 +1,15 @@
+#include<stdio.h>
+
+/**
+ * main - Prints the storage size of the various datatypes
+ * Return: 0 if the function runs successful.
+**/
+int main(void)
+{
+printf("Size of a char: %d \n", sizeof(char));
+printf("Size of an int: %d \n", sizeof(int));
+printf("Size of a long int: %d \n", sizeof(long int));
+printf("Size of a long long int: %d \n", sizeof(long long int));
+printf("Size of a float: %d \n", sizeof(float));
+return (0);
+}
