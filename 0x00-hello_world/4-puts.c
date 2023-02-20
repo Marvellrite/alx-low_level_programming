b9d8ad8c2b9b24d5931bd2089241eb6b0fc1308b
Marvel_ALX <marvelozuru087@gmail.com>
2023-02-19 22:58:41 -0800

    Correct the 4-put.c file

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,11 @@
 #include<stdio.h>
 
+/**
+* main - Prints a string.
+* Return: 0 if the function runs successfully.
+**/
 int main(void)
 {
-  puts("\"Programming is like building a multilingual puzzle");
-    return 0;
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
