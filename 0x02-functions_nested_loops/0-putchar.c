bebbfbc76c4a3533fa9fd1e0fd8f8e4f9bed0c6d
Marvel_ALX <marvelozuru087@gmail.com>
2023-02-21 14:19:59 -0800

    Correct the 0-putchar.c file

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,11 +1,19 @@
-#include<stdio.h>
+#include"main.h"
 
 /*
-* main - Entry point.
+* main - Entry point and also tests the main.h.header file..
 * Return: 0 always if successful.
 */
 int main(void) 
 {
-puts("_putchar");
+_putchar('_');
+_putchar('p');
+_putchar('u');
+_putchar('t');
+_putchar('c');
+_putchar('h');
+_putchar('a');
+_putchar('r');
+_putchar('\n');
 return (0);
 }
