8c48c37ca3947e14cdeabf59f3eb959e0a584f91
Marvel_ALX <marvelozuru087@gmail.com>
2023-02-22 03:25:41 -0800

    Correct 0-putchar.c program

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,10 +1,10 @@
 #include"main.h"
 
-/*
+/**
 * main - Entry point and also tests the main.h.header file..
 * Return: 0 always if successful.
-*/
-int main(void) 
+**/
+int main(void)
 {
 _putchar('_');
 _putchar('p');
