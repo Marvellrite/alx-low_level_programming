97e7a0b89d05de5b4e11fbf1b1f6cf1336e01d0b
Marvel_ALX <marvelozuru087@gmail.com>
2023-02-23 04:00:30 -0800

    Write the 4-print_most_numbers.c program file

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -1,7 +1,7 @@
 #include"main.h"
 
 /**
-* print_numbers - Prints the numbers from 0 to 9
+* print_most_numbers - Prints the numbers from 0 to 9
 * excluding 2 and 4 followed by a new line.
 */
 void print_most_numbers(void)
@@ -12,7 +12,7 @@ for (count = 48; (count >= 48 && count <= 57);
 count++)
 {
 int letter = count;
-  
+
 if (count == 50 || count == 52)
 letter = 0;
 _putchar(letter);
