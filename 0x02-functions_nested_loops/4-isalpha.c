9b13b99bbed47a53a5c5d87ea60194ca430f8ff0
Marvel_ALX <marvelozuru087@gmail.com>
2023-02-22 16:24:35 -0800

    Write the 4-isalpha.c program file

--- 0x02-functions_nested_loops/4-isalpha.c
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -1,14 +1,16 @@
 #include"main.h"
 
 /**
-* _islower - Checks wether a character is lower or not.
-*  Return: 1 if c is a letter and 0 if it is not.
-* @c: The numeric value of the letter to be checked.
+* _isalpha - Checks wether a character is lower or not.
+*@c: The numeric value of the letter to be checked.
+*
+* Return: 1 if c is a letter and 0 if it is not.
 **/
 int _isalpha(int c)
 {
 char character = c;
- if ((character >= 97 && character <= 122) || (character >= 65 && character <= 90))
+if ((character >= 97 && character <= 122) ||
+(character >= 65 && character <= 90))
 return (1);
 else
 return (0);
